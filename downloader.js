9330b2bf2a24f8b48e7d371c158636b2d1bfd2df
Erwan Normand <normand.erwan@gmail.com>
2016-05-29 01:13:58 -0400

    feat(downloader.js): get the urls of the full size wallpapers

--- downloader.js
+++ downloader.js
@@ -6,43 +6,49 @@ var request = require('request'),
 var downloader = module.exports;
 
 downloader.updateWallpapers = function() {
-
-	var feed = getFeed500px('popular');
+	var feed = get500pxFeed('popular');
 
 	request.get(feed.url, function(error, response, body) {
-		if (error) {
-			console.error('Error: failed to retrieve "' + feed.url + '" (error code: ' + error.code + ')');
+		if (handleRequestErrors(error, response) !== 0) {
 			return 1;
 		}
 
-		if (response.statusCode != 200) {
-			console.error('Error: failed to retrieve "' + feed.url + '" (status code: ' + response.statusCode + ')');
-			return 1;
-		}
-
-		feed.parser.write(body);
-		feed.parser.end();
-
-		//console.log(feed.wallpaperUrls);
+		feed.getWallpaperUrls(body, function(wallpaperUrls) {
+			console.log(wallpaperUrls);
+		});
 	});
 
 	return 0;
 };
 
-function getFeed500px(type) {
-	
-	var url = 'http://500px.com/' + type + '.rss';
+function handleRequestErrors(error, response) {
+	if (error) {
+		console.error('Error: failed to retrieve "' + error.host + '" (error code: ' + error.code + ')');
+		return 1;
+	}
 
-	var wallpaperUrls = [];
+	if (response.statusCode != 200) {
+		console.error('Error: failed to retrieve "' + response.toJSON().request.headers.referer + '" (status code: ' + response.statusCode + ')');
+		return 1;
+	}
+
+	return 0;
+}
+
+function get500pxFeed(type) {
+	
+	var feedUrl = 'http://500px.com/' + type + '.rss';
+
+	var wallpaperPageUrls = [];
 
 	var parserLastTag = '';
-	var parser = new htmlparser.Parser({
+	var feedParser = new htmlparser.Parser({
 		onopentag: function(name, attribs) {
 			parserLastTag = name;
 		},
 		ontext: function(text) {
-			if (parserLastTag == 'guid') {
-				wallpaperUrls.push(text);
+			if (parserLastTag === 'guid') {
+				wallpaperPageUrls.push(text);
 			}
 		},
 		onclosetag: function(name) {
@@ -50,9 +56,31 @@ function getFeed500px(type) {
 		}
 	});
 
+	function getWallpaperUrls(feedBody, callback) {
+		feedParser.write(feedBody);
+		feedParser.end();
+
+		var wallpapersUrl = [];
+		wallpaperPageUrls.forEach(function(wallpaperPageUrl) {
+			request.get(wallpaperPageUrl, function(error, response, body) {
+				if (handleRequestErrors(error, response) !== 0) {
+					return 1;
+				}
+
+				var wallpaperUrlPattern = new RegExp('https:\\\/\\\/[a-zA-Z0-9.%_\\\/]+m%3D2000[a-z0-9\\\/]+');
+				var wallpaperUrlMatch = body.match(wallpaperUrlPattern);
+
+				if (wallpaperUrlMatch !== null) {
+					wallpapersUrl.push(wallpaperUrlMatch[0]);
+				}
+			});
+		});
+
+		callback(wallpapersUrl);
+	}
+
 	return {
-		url: url,
-		parser: parser,
-		wallpaperUrls: wallpaperUrls
+		url: feedUrl,
+		getWallpaperUrls: getWallpaperUrls
 	};
 }
